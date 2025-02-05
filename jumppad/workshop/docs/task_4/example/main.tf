6224c69a66343d8e1e2bfd55e658354d09feec71
Simon Lynch <simon.lynch@hashicorp.com>
2025-02-05 17:43:20 +1100

    add: define variables and resources for AAP Minecraft server setup in main.tf

--- jumppad/workshop/docs/task_4/example/main.tf
+++ jumppad/workshop/docs/task_4/example/main.tf
@@ -13,3 +13,36 @@ provider "aap" {
   password             = var.aap_password
   insecure_skip_verify = true
 }
+
+variable "aap_username" {
+  type = string
+}
+
+variable "aap_password" {
+  type = string
+}
+
+
+variable "minecraft_hostname" {
+  type = string
+}
+
+
+resource "aap_inventory" "shared_minecraft" {
+  name = "minecraft"
+  description = "Shared Minecraft Server"
+}
+
+
+# Create a new AAP host for the miecraft server
+resource "aap_host" "vm_hosts" {
+  inventory_id = aap_inventory.shared_minecraft.id
+  name         = each.value.hostname  # Use the hostname for each VM
+  variables    = jsonencode({
+    "ansible_host"     : "${var.minecraft_hostname}" #ip address of the VM
+})
+
+
+# Execute the Job Template to update the minecraft server for access using existing job template - minecraft_whitelist
+# note: the job template should be created in the AAP server and is already associated with the machine credentials configured in task_2
+
